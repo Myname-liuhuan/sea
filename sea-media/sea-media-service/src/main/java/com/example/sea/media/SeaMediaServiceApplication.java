d039cfa25332b56490bb42b9e986808495a111ad
liuhuan <1171814970@qq.com>
2025-02-09 19:45:47 +0800

    fix:模块添加@EnableDiscoveryClient注册与发现注解

--- sea-media/sea-media-service/src/main/java/com/example/sea/media/SeaMediaServiceApplication.java
+++ sea-media/sea-media-service/src/main/java/com/example/sea/media/SeaMediaServiceApplication.java
@@ -3,8 +3,10 @@ package com.example.sea.media;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("com.example.sea.media.dao")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SeaMediaServiceApplication {
 
