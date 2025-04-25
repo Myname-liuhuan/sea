3ad22fb81b90cbedea3d933e96b0c657654c8cb6
Henry <studyliuhuan@163.com>
2025-04-25 15:11:02 +0800

    fix: 返回给前端的VO，id转为字符串，时间类型格式化为yyyy-MM-dd HH:mm:ss

--- sea-code/src/main/java/com/example/sea/code/common/base/BaseVO.java
+++ sea-code/src/main/java/com/example/sea/code/common/base/BaseVO.java
@@ -2,6 +2,10 @@ package com.example.sea.code.common.base;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
 import lombok.Data;
 
 /**
@@ -14,15 +18,18 @@ public class BaseVO {
     /**
      * 主键 ID
      */
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
 
     /**
      * 创建时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime createTime;
 
     /**
      * 更新时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime updateTime;
 }
