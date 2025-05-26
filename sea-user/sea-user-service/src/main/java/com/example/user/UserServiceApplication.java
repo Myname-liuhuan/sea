0b1d9e52e11e4855dd821f26e787da3c8af30e6c
Henry <studyliuhuan@163.com>
2025-05-26 17:44:28 +0800

    feat: 添加user模块

---
+++ sea-user/sea-user-service/src/main/java/com/example/user/UserServiceApplication.java
@@ -0,0 +1,17 @@
+package com.example.user;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@MapperScan("com.example.sea.user.dao")
+@EnableDiscoveryClient
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
