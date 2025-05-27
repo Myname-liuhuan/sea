eda48a572945bf98ac72e864055ccc474104cf97
Henry <studyliuhuan@163.com>
2025-05-27 09:45:35 +0800

    refactor: 修改模块名称user为system

--- sea-system/sea-system-service/src/main/java/com/example/system/SystemServiceApplication.java
+++ sea-system/sea-system-service/src/main/java/com/example/system/SystemServiceApplication.java
@@ -1,17 +1,17 @@
-package com.example.user;
+package com.example.system;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@MapperScan("com.example.sea.user.dao")
+@MapperScan("com.example.sea.system.dao")
 @EnableDiscoveryClient
 @SpringBootApplication
-public class UserServiceApplication {
+public class SystemServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		SpringApplication.run(SystemServiceApplication.class, args);
 	}
 
 }
