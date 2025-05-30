718d5bf34c912c9b693fbb5d589150eb2f0babbf
liuhuan <1171814970@qq.com>
2025-05-30 20:27:17 +0800

    fix: 依赖不全问题

--- sea-gateway/src/main/java/com/example/sea/gateway/filter/AuthFilter.java
+++ sea-gateway/src/main/java/com/example/sea/gateway/filter/AuthFilter.java
@@ -2,13 +2,20 @@ package com.example.sea.gateway.filter;
 
 import java.nio.charset.StandardCharsets;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.server.ServerWebExchange;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 
 import com.example.sea.gateway.properties.SecurityWhiteListProperties;
 
@@ -65,7 +72,7 @@ public class AuthFilter implements GlobalFilter, Ordered  {
 
     private boolean isWhiteListed(String path) {
         AntPathMatcher pathMatcher = new AntPathMatcher();
-        return whiteListConfig.getWhitelist().stream()
+        return securityWhiteListProperties.getWhitelist().stream()
                 .anyMatch(pattern -> pathMatcher.match(pattern, path));
     }
 
